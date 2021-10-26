ddcab44c9b939bb99ca67338a9aa0eccd18dd500
wrightsman-nick <77906618+wrightsman-nick@users.noreply.github.com>
2021-10-26 16:14:53 -0400

    jump

--- Wrightsman-2D-Week/Assets/Scripts/Player/PlayerController.cs
+++ Wrightsman-2D-Week/Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    Rigidbody2D rigidBody2D;
+
+    public float runSpeed = 5f;
+    public float jumpSpeed = 200f;
+
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector2 direction = new Vector2(horizontalInput * runSpeed * Time.deltaTime, 0);
+
+        rigidBody2D.AddForce(direction);
+
+
+    }
+
+    void Jump()
+    {
+        rigidBody2D.velocity = new Vector2(rigidBody2D.velocity.x, jumpSpeed);
     }
 }
